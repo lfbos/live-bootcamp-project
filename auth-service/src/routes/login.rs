1e9b35dd275e1d4f07e8a06207787c03f0cdc290
Luis Boscan <lboscannava@gmail.com>
2025-08-30 10:43:13 -0500

    Utils added and jwt features

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,33 +1,45 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 
 use crate::{
     app_state::AppState,
     domain::{AuthAPIError, Email, Password},
+    utils::auth::generate_auth_cookie,
 };
 
 pub async fn login(
     State(state): State<AppState>,
+    jar: CookieJar,
     Json(request): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
-    let email =
-        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
-    let password =
-        Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let email = match Email::parse(request.email.clone()) {
+        Ok(email) => email,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
+    let password = match Password::parse(request.password.clone()) {
+        Ok(password) => password,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
 
     let user_store = state.user_store.read().await;
 
-    if !user_store.validate_user(&email, &password).await.is_ok() {
-        return Err(AuthAPIError::IncorrectCredentials);
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return (jar, Err(AuthAPIError::IncorrectCredentials));
     }
 
-    user_store.get_user(&email).await.map_err(|_| AuthAPIError::IncorrectCredentials)?;
+    let auth_cookie = match generate_auth_cookie(&email) {
+        Ok(cookie) => cookie,
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+    };
+
+    let updated_jar = jar.add(auth_cookie);
 
     let response = Json(LoginResponse {
         message: "Login successful!".to_string(),
     });
 
-    Ok((StatusCode::OK, response.into_response()))
+    (updated_jar, Ok((StatusCode::OK, response)))
 }
 
 #[derive(Deserialize)]
