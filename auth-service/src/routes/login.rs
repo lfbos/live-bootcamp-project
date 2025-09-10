43fd74c8bfbcd336102dea2447b0c232a98909fd
Luis Boscan <lboscannava@gmail.com>
2025-09-10 18:19:52 -0500

    2FA structs

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -28,14 +28,15 @@ pub async fn login(
         return (jar, Err(AuthAPIError::IncorrectCredentials));
     }
 
-    let auth_cookie = match generate_auth_cookie(&email) {
-        Ok(cookie) => cookie,
+    let user = match user_store.get_user(&email).await {
+        Ok(user) => user,
         Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
     };
 
-    let updated_jar = jar.add(auth_cookie);
-
-    (updated_jar, Ok(StatusCode::OK.into_response()))
+    match user.requires_2fa {
+        true => handle_2fa(jar).await,
+        false => handle_no_2fa(&user.email, jar).await,
+    }
 }
 
 #[derive(Deserialize)]
@@ -44,7 +45,55 @@ pub struct LoginRequest {
     password: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct LoginResponse {
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+pub enum LoginResponse {
+    RegularAuth,
+    TwoFactorAuth(TwoFactorAuthResponse),
+}
+
+// If a user requires 2FA, this JSON body should be returned!
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TwoFactorAuthResponse {
     pub message: String,
+    #[serde(rename = "loginAttemptId")]
+    pub login_attempt_id: String,
+}
+
+async fn handle_2fa(
+    jar: CookieJar,
+) -> (
+    CookieJar,
+    Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
+) {
+    (
+        jar,
+        Ok((
+            StatusCode::PARTIAL_CONTENT,
+            Json(LoginResponse::TwoFactorAuth(TwoFactorAuthResponse {
+                message: "2FA required".to_owned(),
+                login_attempt_id: "123456".to_owned(),
+            })),
+        )),
+    )
+}
+
+async fn handle_no_2fa(
+    email: &Email,
+    jar: CookieJar,
+) -> (
+    CookieJar,
+    Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
+) {
+    let auth_cookie = match generate_auth_cookie(&email) {
+        Ok(cookie) => cookie,
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+    };
+
+    let updated_jar = jar.add(auth_cookie);
+
+    (
+        updated_jar,
+        Ok((StatusCode::OK, Json(LoginResponse::RegularAuth))),
+    )
 }
--- auth-service/src/services/mod.rs
+++ auth-service/src/services/mod.rs
@@ -1,1 +1,2 @@
 pub mod hashmap_user_store;
+pub mod hashmap_two_fa_code_store;
